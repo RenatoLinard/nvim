9db3ce269ad4c65647bcf0d9fbbe16f8c52d2314
RenatoLinard <renatolinardjr@gmail.com>
2023-07-17 12:21:33 -0400

    instalação e configuração lualine

--- lua/renatolinardjr/packer.lua
+++ lua/renatolinardjr/packer.lua
@@ -19,6 +19,10 @@ return require('packer').startup(function()
 	  end
   })
 
+  use {
+      'nvim-lualine/lualine.nvim',
+      requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+  }
 
   use({
       "folke/trouble.nvim",
